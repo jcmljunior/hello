bcb5bb3f8daba68b6cc2b4029ca8189facd2cc68
Julio <165454530+jcmljunior@users.noreply.github.com>
2024-12-03 02:11:55 -0300

    Alteração de PackedScene para String
    A Godot não tem a capacidade de exportar PackedScene de componentes de componentes. solução alternativa implementada.

--- features/gui/scripts/settings_button.gd
+++ features/gui/scripts/settings_button.gd
@@ -1,11 +1,12 @@
 extends CoreButton
 
-@export var target_scene: PackedScene
+# A Godot aparentemente não permite criar componentes de componentes referênciando cenas.
+# Solução alternativa temporaria.
+@export_file("*.tscn") var target_scene: String
 
 func _on_pressed() -> void:
-	if not target_scene or not target_scene.can_instantiate():
-		print_debug("Oppss, ocorreu um erro ao alterar a cena.")
+	if not target_scene:
 		return
 	
 	print_debug("Configurando...")
-	get_tree().change_scene_to_packed(target_scene)
+	get_tree().change_scene_to_file(target_scene)
