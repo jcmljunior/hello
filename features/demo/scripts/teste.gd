c58cbf65164c453a3341c671c27d1ab0d2e2b461
Julio <165454530+jcmljunior@users.noreply.github.com>
2024-12-03 01:48:18 -0300

    Implementação do sistema de pontuação

--- features/demo/scripts/teste.gd
+++ features/demo/scripts/teste.gd
@@ -1,6 +1,10 @@
 extends Control
 
 @export var back_scene : PackedScene
+@onready var label: Label = %Label
+
+func _ready() -> void:
+	AutoloadManager.connect("on_score_changed", _on_score_changed)
 
 func _on_button_pressed() -> void:
 	if not back_scene or not back_scene.can_instantiate():
@@ -8,3 +12,13 @@ func _on_button_pressed() -> void:
 		return
 		
 	get_tree().change_scene_to_packed(back_scene)
+
+
+func _on_score_button_pressed() -> void:
+	AutoloadManager.add_score()
+
+func _on_decrement_button_pressed() -> void:
+	AutoloadManager.remove_score()
+
+func _on_score_changed() -> void:
+	label.text = str(AutoloadManager.get_score())
