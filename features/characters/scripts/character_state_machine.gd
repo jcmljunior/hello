9cff73911b18e74b4c5dc09912f78fbff0092081
Julio <165454530+jcmljunior@users.noreply.github.com>
2024-12-06 17:00:07 -0300

    Ajuste para execução do pulo via joypad

--- features/characters/scripts/character_state_machine.gd
+++ features/characters/scripts/character_state_machine.gd
@@ -14,7 +14,7 @@ func _process(delta: float) -> void:
 	if not player.can_moviment and player.velocity.y == 0:
 		player._change_state_handler("spawn")
 	
-	elif Input.is_action_just_pressed("ui_accept") and player.velocity.y <= 0 and player.can_moviment:
+	elif Input.is_action_just_pressed("jump") and player.velocity.y <= 0 and player.can_moviment:
 		player._change_state_handler("jump")
 		
 	elif player.direction and player.is_on_floor() and player.can_moviment:
@@ -23,5 +23,5 @@ func _process(delta: float) -> void:
 	elif not player.direction and player.is_on_floor() and player.can_moviment:
 		player._change_state_handler("idle")
 	
-	elif player.velocity.y >= 0 :
+	elif player.velocity.y >= 0 and player.can_moviment:
 		player._change_state_handler("land")
